e056256e0e69a8c77f77de41c818d594f82f16a7
J.C. Moyer <jcmoyer32@gmail.com>
2021-12-11 01:32:43 -0500

    aoc2021: day 11: reduce allocation in loops

--- AdventOfCode2021/src/day11.cpp
+++ AdventOfCode2021/src/day11.cpp
@@ -4,45 +4,63 @@
 
 #include <sr/sr.hpp>
 
-void flash(sr::array2d<int>& tm, sr::vec2i p, std::unordered_set<sr::vec2i>& flashed) {
-    flashed.insert(p);
-    sr::for_neighbors8(tm, p, [&](int& e, auto p) {
-        ++e;
-    });
-}
+class world {
+public:
+    world(sr::array2d<int> m) {
+        reset(std::move(m));
+    }
 
-int count_flashable(sr::array2d<int>& tm, std::unordered_set<sr::vec2i>& flashed) {
-    int count = 0;
-    sr::for_each_2d(tm, [&](int& e, auto p) {
-        if (!flashed.contains(p) && e > 9)
-            ++count;
-    });
-    return count;
-}
+    void reset(sr::array2d<int> m) {
+        octopi = std::move(m);
+        flashed.reserve(octopi.size());
+        flashable.reserve(octopi.size());
+    }
 
-int tick(sr::array2d<int>& m) {
-    std::unordered_set<sr::vec2i> flashed;
+    int tick() {
+        flashed.clear();
+        flashable.clear();
 
-    int count = 0;
+        int count = 0;
 
-    for (int& e : m)
-        ++e;
+        for (int& e : octopi)
+            ++e;
 
-    while (count_flashable(m, flashed)) {
-        sr::for_each_2d(m, [&](int& e, auto p) {
-            if (e > 9 && !flashed.count(p)) {
-                flash(m, p, flashed);
-                ++count;
+        while (get_flashable()) {
+            for (auto& point : flashable) {
+                flash(point);
             }
+            count += flashable.size();
+        }
+
+        for (auto p : flashed) {
+            octopi.at(p) = 0;
+        }
+
+        return count;
+    }
+
+private:
+    void flash(sr::vec2i p) {
+        flashed.insert(p);
+        sr::for_neighbors8(octopi, p, [&](int& e, auto p) {
+            ++e;
         });
     }
 
-    for (auto p : flashed) {
-        m.at(p) = 0;
+    bool get_flashable() {
+        flashable.clear();
+        sr::for_each_2d(octopi, [&](int& e, auto p) {
+            if (!flashed.contains(p) && e > 9)
+                flashable.push_back(p);
+        });
+        return flashable.size();
     }
 
-    return count;
-}
+private:
+    sr::array2d<int> octopi;
+    std::unordered_set<sr::vec2i> flashed;
+    std::vector<sr::vec2i> flashable;
+};
 
 sr::array2d<int> to_int_map(const sr::array2d<char>& m) {
     sr::array2d<int> out(m.width(), m.height());
@@ -56,21 +74,19 @@ int main(int argc, char* argv[]) {
     auto args = sr::parse_command_line(argc, argv);
     std::ifstream input(args.input_filename);
     auto char_tilemap = sr::read_tilemap(input);
-
-    sr::array2d<int> octos;
+    world w(to_int_map(char_tilemap));
 
     // part 1
-    octos = to_int_map(char_tilemap);
     int flashes = 0;
     for (int i = 0; i < 100; ++i) {
-        flashes += tick(octos);
+        flashes += w.tick();
     }
     sr::solution(flashes);
 
     // part 2
-    octos = to_int_map(char_tilemap);
+    w.reset(to_int_map(char_tilemap));
     for (int i = 0;; ++i) {
-        if (tick(octos) == 100) {
+        if (w.tick() == 100) {
             sr::solution(1 + i);
             break;
         }
