0857955ddc3906d76894eef67d1d274d274e4485
J.C. Moyer <jcmoyer32@gmail.com>
2021-12-15 12:39:26 -0500

    Use generic A* from sr

--- AdventOfCode2021/src/day15.cpp
+++ AdventOfCode2021/src/day15.cpp
@@ -6,81 +6,32 @@
 
 using risk_map_type = sr::array2d<uint8_t>;
 
-size_t heuristic(const risk_map_type& riskmap, sr::vec2i from, sr::vec2i to) {
-    return std::abs(riskmap.at(from) - riskmap.at(to));
-}
+struct controller {
+    int heuristic(const risk_map_type& a, sr::vec2i from, sr::vec2i to) {
+        return sr::manhattan(to, sr::vec2i{(int)a.width() - 1, (int)a.height() - 1});
+    }
 
-struct path_score {
-    size_t g_score, f_score;
-    sr::vec2i from;
+    int weight(const risk_map_type& a, sr::vec2i from, sr::vec2i to) {
+        return a[to];
+    }
 
-    static constexpr path_score infinity() {
-        return {std::numeric_limits<size_t>::max(), std::numeric_limits<size_t>::max()};
+    template <typename Buffer>
+    void neighbors(const risk_map_type& a, sr::vec2i pos, Buffer& buf) {
+        sr::for_neighbors4(a, pos, [&](auto, auto npos) {
+            buf.push_back(npos);
+        });
     }
 };
 
-size_t sum_risk(const risk_map_type& riskmap, const sr::array2d<path_score>& pathmap) {
+size_t solve(const risk_map_type& riskmap) {
     sr::vec2i start{};
     sr::vec2i end{(int)riskmap.width() - 1, (int)riskmap.height() - 1};
-
-    auto path_cur = end;
-    size_t total_risk = 0;
-    while (path_cur != start) {
-        total_risk += riskmap.at(path_cur);
-        path_cur = pathmap[path_cur].from;
+    std::vector<sr::vec2i> path = sr::astar(riskmap, start, end, controller{});
+    size_t result = 0;
+    for (size_t i = 1; i < path.size(); ++i) {
+        result += riskmap[path[i]];
     }
-    return total_risk;
-}
-
-// implementation based on pseudocode from https://en.wikipedia.org/wiki/A*_search_algorithm
-size_t astar(const risk_map_type& riskmap) {
-    sr::vec2i start{};
-    sr::vec2i end{(int)riskmap.width() - 1, (int)riskmap.height() - 1};
-
-    std::vector<sr::vec2i> frontier;
-    frontier.reserve(riskmap.size());
-    // used for O(1) lookup
-    std::unordered_set<sr::vec2i> frontier_set;
-    frontier_set.reserve(riskmap.size());
-
-    frontier.push_back(start);
-    frontier_set.insert(start);
-
-    sr::array2d<path_score> score_map(riskmap.width(), riskmap.height(), path_score::infinity());
-    score_map[start] = path_score{0, 0};
-
-    auto heap_pred = [&](auto&& v0, auto&& v1) {
-        // operator> causes the heap to be a min-heap
-        return score_map[v0].f_score > score_map[v1].f_score;
-    };
-
-    while (frontier.size()) {
-        auto current = frontier.front();
-
-        if (current == end)
-            return sum_risk(riskmap, score_map);
-
-        std::ranges::pop_heap(frontier, heap_pred);
-        frontier.pop_back();
-        frontier_set.erase(current);
-
-        sr::for_neighbors4(riskmap, current, [&](uint8_t val, sr::vec2i npos) {
-            auto tentative_score = score_map[current].g_score + val;
-            size_t nscore = score_map[npos].g_score;
-            if (tentative_score < nscore) {
-                score_map[npos].from = current;
-                score_map[npos].g_score = tentative_score;
-                score_map[npos].f_score = tentative_score + heuristic(riskmap, current, npos);
-                if (!frontier_set.contains(npos)) {
-                    frontier.push_back(npos);
-                    std::ranges::push_heap(frontier, heap_pred);
-                    frontier_set.insert(npos);
-                }
-            }
-        });
-    }
-
-    throw std::runtime_error("no path to end");
+    return result;
 }
 
 int main(int argc, char* argv[]) {
@@ -94,7 +45,7 @@ int main(int argc, char* argv[]) {
         char* ch = map.data() + i;
         *(risk_map.data() + i) = *ch - '0';
     }
-    sr::solution(astar(risk_map));
+    sr::solution(solve(risk_map));
 
     risk_map_type big_risk_map(map.width() * 5, map.height() * 5);
     for (int y = 0; y < 5; ++y) {
@@ -117,6 +68,7 @@ int main(int argc, char* argv[]) {
             }
         }
     }
-    sr::solution(astar(big_risk_map));
+    sr::solution(solve(big_risk_map));
+
     return 0;
 }
