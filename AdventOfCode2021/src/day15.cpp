45fd7a0204fd34307d1b99004560bc7059d3c607
J.C. Moyer <jcmoyer32@gmail.com>
2021-12-15 02:54:27 -0500

    aoc2021: optimize day 15

--- AdventOfCode2021/src/day15.cpp
+++ AdventOfCode2021/src/day15.cpp
@@ -10,73 +10,78 @@ size_t heuristic(const risk_map_type& riskmap, sr::vec2i from, sr::vec2i to) {
     return std::abs(riskmap.at(from) - riskmap.at(to));
 }
 
-// more or less copied from https://en.wikipedia.org/wiki/A*_search_algorithm
+struct path_score {
+    size_t g_score, f_score;
+
+    static constexpr path_score infinity() {
+        return {std::numeric_limits<size_t>::max(), std::numeric_limits<size_t>::max()};
+    }
+};
+
+size_t sum_risk(const risk_map_type& riskmap, const sr::array2d<sr::vec2i>& pathmap) {
+    sr::vec2i start{};
+    sr::vec2i end{(int)riskmap.width() - 1, (int)riskmap.height() - 1};
+
+    auto path_cur = end;
+    size_t total_risk = 0;
+    while (path_cur != start) {
+        total_risk += riskmap.at(path_cur);
+        path_cur = pathmap[path_cur];
+    }
+    return total_risk;
+}
+
+// implementation based on pseudocode from https://en.wikipedia.org/wiki/A*_search_algorithm
 size_t astar(const risk_map_type& riskmap) {
     sr::vec2i start{};
     sr::vec2i end{(int)riskmap.width() - 1, (int)riskmap.height() - 1};
 
     std::vector<sr::vec2i> frontier;
     frontier.reserve(riskmap.size());
+    // used for O(1) lookup
+    std::unordered_set<sr::vec2i> frontier_set;
+    frontier_set.reserve(riskmap.size());
 
     frontier.push_back(start);
+    frontier_set.insert(start);
 
-    std::unordered_map<sr::vec2i, sr::vec2i> came_from;
-    came_from.reserve(riskmap.size());
+    sr::array2d<sr::vec2i> came_from(riskmap.width(), riskmap.height());
 
-    std::unordered_map<sr::vec2i, size_t> g_score_map;
-    g_score_map.reserve(riskmap.size());
-    g_score_map[start] = 0;
-
-    std::unordered_map<sr::vec2i, size_t> f_score_map;
-    f_score_map.reserve(riskmap.size());
-    f_score_map[start] = 0;
+    sr::array2d<path_score> score_map(riskmap.width(), riskmap.height(), path_score::infinity());
+    score_map[start] = path_score{0, 0};
 
     auto heap_pred = [&](auto&& v0, auto&& v1) {
-        size_t v0_score = -1;
-        if (auto it = f_score_map.find(v0); it != f_score_map.end()) {
-            v0_score = it->second;
-        }
-        size_t v1_score = -1;
-        if (auto it = f_score_map.find(v1); it != f_score_map.end()) {
-            v1_score = it->second;
-        }
         // operator> causes the heap to be a min-heap
-        return v0_score > v1_score;
+        return score_map[v0].f_score > score_map[v1].f_score;
     };
 
     while (frontier.size()) {
         auto current = frontier.front();
 
         if (current == end)
-            break;
+            return sum_risk(riskmap, came_from);
 
         std::ranges::pop_heap(frontier, heap_pred);
         frontier.pop_back();
+        frontier_set.erase(current);
 
-        sr::for_neighbors4(riskmap, current, [&](int val, sr::vec2i npos) {
-            auto tentative_score = g_score_map[current] + val;
-            size_t nscore = -1;
-            if (g_score_map.contains(npos)) {
-                nscore = g_score_map[npos];
-            }
+        sr::for_neighbors4(riskmap, current, [&](uint8_t val, sr::vec2i npos) {
+            auto tentative_score = score_map[current].g_score + val;
+            size_t nscore = score_map[npos].g_score;
             if (tentative_score < nscore) {
                 came_from[npos] = current;
-                g_score_map[npos] = tentative_score;
-                f_score_map[npos] = tentative_score + heuristic(riskmap, current, npos);
-                if (std::ranges::find(frontier, npos) == frontier.end()) {
+                score_map[npos].g_score = tentative_score;
+                score_map[npos].f_score = tentative_score + heuristic(riskmap, current, npos);
+                if (!frontier_set.contains(npos)) {
                     frontier.push_back(npos);
                     std::ranges::push_heap(frontier, heap_pred);
+                    frontier_set.insert(npos);
                 }
             }
         });
     }
-    auto path_cur = end;
-    size_t total_risk = 0;
-    while (came_from.contains(path_cur)) {
-        total_risk += riskmap.at(path_cur);
-        path_cur = came_from[path_cur];
-    }
-    return total_risk;
+
+    throw std::runtime_error("no path to end");
 }
 
 int main(int argc, char* argv[]) {
