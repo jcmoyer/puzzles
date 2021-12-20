e274fa10fe8ae8c84e7dd5cf99c093de3cf821d9
J.C. Moyer <jcmoyer32@gmail.com>
2021-12-20 12:02:12 -0500

    aoc2021: day 20: various optimizations
    - Use a set containing only lit pixels
    - Pre-allocate space for 20k pixels
    - Reduce bounds calculation to one pass
    - Don't reset sim for part 2
    
    This brings the runtime from ~440ms to ~300ms.

--- AdventOfCode2021/src/day20.cpp
+++ AdventOfCode2021/src/day20.cpp
@@ -2,6 +2,10 @@
 
 struct image_bounds {
     sr::vec2i min, max;
+
+    bool contains(const sr::vec2i& p) const {
+        return p.x() >= min.x() && p.y() >= min.y() && p.x() <= max.x() && p.y() <= max.y();
+    }
 };
 
 class image {
@@ -11,67 +15,79 @@ public:
     image(const sr::array2d<char>& data) {
         for (int y = 0; y < data.height(); ++y) {
             for (int x = 0; x < data.width(); ++x) {
-                set({x, y}, data.at(x, y) == '#');
+                if (data.at(x, y) == '#')
+                    set({x, y});
             }
         }
+        bounds = sim_bounds();
     }
 
-    void set(sr::vec2i p, bool state) {
-        pixels[p] = state;
+    void set(const sr::vec2i& p) {
+        pixels.insert(p);
     }
 
-    bool is_light(sr::vec2i p) const {
-        if (auto it = pixels.find(p); it != pixels.end())
-            return it->second;
+    bool is_light(const sr::vec2i& p) const {
+        if (pixels.contains(p))
+            return true;
+        if (bounds.contains(p))
+            return false;
         return space_bit;
     }
 
-    uint32_t index_at(sr::vec2i p) const {
-        uint32_t index = 0;
+    uint16_t index_at(const sr::vec2i& p) const {
+        uint16_t index = 0;
         for (int yo = -1; yo <= 1; ++yo) {
             for (int xo = -1; xo <= 1; ++xo) {
                 index <<= 1;
-                index |= static_cast<uint32_t>(is_light(p + sr::vec2i{xo, yo}));
+                index |= static_cast<uint16_t>(is_light(p + sr::vec2i{xo, yo}));
             }
         }
         return index;
     }
 
     image_bounds sim_bounds() const {
-        auto [minx, maxx] = std::ranges::minmax_element(pixels, [](auto&& p0, auto&& p1) {
-            return p0.first.x() < p1.first.x();
-        });
-        auto [miny, maxy] = std::ranges::minmax_element(pixels, [](auto&& p0, auto&& p1) {
-            return p0.first.y() < p1.first.y();
-        });
-        return {
-            {minx->first.x() - 1, miny->first.y() - 1},
-            {maxx->first.x() + 1, maxy->first.y() + 1},
+        image_bounds result{
+            {std::numeric_limits<int>::max(), std::numeric_limits<int>::max()},
+            {std::numeric_limits<int>::min(), std::numeric_limits<int>::min()},
         };
+        for (const auto& p : pixels) {
+            result.min.x() = std::min(result.min.x(), p.x());
+            result.min.y() = std::min(result.min.y(), p.y());
+            result.max.x() = std::max(result.max.x(), p.x());
+            result.max.y() = std::max(result.max.y(), p.y());
+        }
+        result.min -= {1, 1};
+        result.max += {1, 1};
+        return result;
     }
 
-    int count_lit() const {
-        return (int)std::ranges::count_if(pixels, [](auto&& p) {
-            return p.second;
-        });
+    size_t count_lit() const {
+        return pixels.size();
     }
 
     void enhance(const sr::dynamic_bitset& filter, image& output) const {
         const size_t space_bit_transition = space_bit ? 0b111'111'111 : 0;
         output.space_bit = filter.get(space_bit_transition);
+        output.pixels.clear();
         image_bounds b = sim_bounds();
         for (int y = b.min.y(); y <= b.max.y(); ++y) {
             for (int x = b.min.x(); x <= b.max.x(); ++x) {
-                uint32_t index = index_at({x, y});
-                output.set({x, y}, filter.get(index));
+                if (filter.get(index_at({x, y})))
+                    output.set({x, y});
             }
         }
+        output.bounds = b;
+    }
+
+    void reserve(size_t count) {
+        pixels.reserve(count);
     }
 
 private:
-    std::unordered_map<sr::vec2i, bool> pixels;
+    std::unordered_set<sr::vec2i> pixels;
     // whether infinite space is light or dark
     bool space_bit = false;
+    image_bounds bounds{};
 };
 
 int main(int argc, char* argv[]) {
@@ -85,7 +101,6 @@ int main(int argc, char* argv[]) {
     std::getline(input, empty);
 
     auto image_data = sr::read_tilemap(input);
-    image init_image(image_data);
 
     sr::dynamic_bitset filter;
     for (char ch : filter_str) {
@@ -93,11 +108,15 @@ int main(int argc, char* argv[]) {
     }
 
     // keep two images and reuse storage
-    image images[2];
+    image images[2]{
+        image(image_data),
+        image(),
+    };
+    images[0].reserve(20000);
+    images[1].reserve(20000);
     size_t dest = 1;
 
     // part 1
-    images[0] = init_image;
     for (int i = 0; i < 2; ++i) {
         images[!dest].enhance(filter, images[dest]);
         dest = !dest;
@@ -105,9 +124,7 @@ int main(int argc, char* argv[]) {
     sr::solution(images[!dest].count_lit());
 
     // part 2
-    images[0] = init_image;
-    dest = 1;
-    for (int i = 0; i < 50; ++i) {
+    for (int i = 0; i < 48; ++i) {
         images[!dest].enhance(filter, images[dest]);
         dest = !dest;
     }
