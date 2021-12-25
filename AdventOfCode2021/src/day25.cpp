739c3a98aa8cd13d70b1010531ceb1dc44d17786
J.C. Moyer <jcmoyer32@gmail.com>
2021-12-25 12:39:11 -0500

    aoc2021: reduce day 25

--- AdventOfCode2021/src/day25.cpp
+++ AdventOfCode2021/src/day25.cpp
@@ -1,58 +1,33 @@
 #include <sr/sr.hpp>
 
-size_t step_ca(sr::array2d<char>& ca, sr::array2d<char>& out) {
+size_t move_element(sr::array2d<char>& ca, char what, const sr::vec2i& offset, sr::array2d<char>& out) {
     size_t moved = 0;
-    for (int y = 0; y < ca.height(); ++y) {
-        for (int x = 0; x < ca.width(); ++x) {
-            if (ca.at(x, y) == '>') {
-                const auto* next = ca.at_opt(x + 1, y);
-                int target_x = x + 1;
-                if (!next) {
-                    next = ca.at_opt(0, y);
-                    target_x = 0;
-                }
-                if (*next == '.') {
-                    out.at(x, y) = '.';
-                    out.at(target_x, y) = '>';
-                    ++moved;
-                }
-            }
+    sr::for_each_2d(ca, [&](char c, const sr::vec2i& p) {
+        sr::vec2i n = p + offset;
+        if (c == what && ca.at_wrap(n) == '.') {
+            out[p] = '.';
+            out.at_wrap(n) = what;
+            ++moved;
         }
-    }
+    });
+    return moved;
+}
+
+size_t step_ca(sr::array2d<char>& ca, sr::array2d<char>& out) {
+    size_t moved = move_element(ca, '>', {1, 0}, out);
     ca = out;
-    for (int y = 0; y < ca.height(); ++y) {
-        for (int x = 0; x < ca.width(); ++x) {
-            if (ca.at(x, y) == 'v') {
-                const auto* next = ca.at_opt(x, y + 1);
-                int target_y = y + 1;
-                if (!next) {
-                    next = ca.at_opt(x, 0);
-                    target_y = 0;
-                }
-                if (*next == '.') {
-                    out.at(x, y) = '.';
-                    out.at(x, target_y) = 'v';
-                    ++moved;
-                }
-            }
-        }
-    }
+    moved += move_element(ca, 'v', {0, 1}, out);
+    ca = out;
     return moved;
 }
 
 int main(int argc, char* argv[]) {
     auto args = sr::parse_command_line(argc, argv);
-    auto tm = sr::read_tilemap(args.get_input_stream());
-    auto out = tm;
+    auto ca = sr::read_tilemap(args.get_input_stream());
+    auto out = ca;
     size_t steps = 0;
-    while (true) {
-        size_t moved = step_ca(tm, out);
+    while (size_t moved = step_ca(ca, out))
         ++steps;
-        if (moved == 0) {
-            break;
-        }
-        tm = out;
-    }
-    sr::solution(steps);
+    sr::solution(steps + 1);
     return 0;
 }
