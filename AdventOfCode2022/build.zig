38a6950381dfd5e73ab71bed1d2338ac14b5da91
J.C. Moyer <jcmoyer32@gmail.com>
2022-12-01 14:28:22 -0500

    Integrate test_runner with zig build

--- AdventOfCode2022/build.zig
+++ AdventOfCode2022/build.zig
@@ -1,4 +1,5 @@
 const std = @import("std");
+const Step = std.build.Step;
 
 const year = 2022;
 
@@ -10,6 +11,16 @@ pub fn build(b: *std.build.Builder) void {
     while (day <= 25) : (day += 1) {
         const day_name = b.fmt("day{d:0>2}", .{day});
         const src_name = b.fmt("src/{s}.zig", .{day_name});
+
+        std.fs.cwd().access(src_name, .{}) catch |err| {
+            if (err == error.FileNotFound) {
+                continue;
+            } else {
+                std.debug.print("Failed to access src_name: {!}", .{err});
+                std.process.exit(1);
+            }
+        };
+
         const exe = b.addExecutable(day_name, src_name);
         exe.setTarget(target);
         exe.setBuildMode(mode);
@@ -26,6 +37,7 @@ pub fn build(b: *std.build.Builder) void {
         });
         // TODO: maybe could get this from aoctool script
         const input_name = b.fmt("test/{s}-input.txt", .{day_name});
+        const output_name = b.fmt("test/{s}-output.txt", .{day_name});
 
         const run_cmd = exe.run();
         run_cmd.addArg(input_name);
@@ -43,5 +55,58 @@ pub fn build(b: *std.build.Builder) void {
         const test_step_desc = b.fmt("Run tests for {s}", .{day_name});
         const test_step = b.step(test_step_name, test_step_desc);
         test_step.dependOn(&tests.step);
+
+        std.fs.cwd().access(output_name, .{}) catch |err| {
+            if (err == error.FileNotFound) {
+                // ok, this puzzle probably wasn't solved yet
+                continue;
+            } else {
+                std.debug.print("Failed to stat output_name: {!}", .{err});
+                std.process.exit(1);
+            }
+        };
+
+        const test_runner = PythonTestRunnerStep.init(b, exe, input_name, output_name);
+        test_step.dependOn(&test_runner.step);
     }
 }
+
+const PythonTestRunnerStep = struct {
+    builder: *std.build.Builder,
+    step: Step,
+    exe_step: *std.build.LibExeObjStep,
+    input_filename: []const u8,
+    output_filename: []const u8,
+
+    fn init(b: *std.build.Builder, exe: *std.build.LibExeObjStep, input_filename: []const u8, output_filename: []const u8) *PythonTestRunnerStep {
+        var self = b.allocator.create(PythonTestRunnerStep) catch unreachable;
+        self.* = .{
+            .builder = b,
+            .step = Step.init(.custom, b.fmt("PythonTestRunnerStep{s}", .{exe.name}), b.allocator, make),
+            .exe_step = exe,
+            .input_filename = input_filename,
+            .output_filename = output_filename,
+        };
+        self.step.dependOn(&exe.step);
+        return self;
+    }
+
+    fn make(step: *Step) anyerror!void {
+        var self = @fieldParentPtr(PythonTestRunnerStep, "step", step);
+        const result = try std.ChildProcess.exec(.{
+            .allocator = self.builder.allocator,
+            .argv = &[_][]const u8{
+                "python",
+                "../scripts/test_runner.py",
+                self.exe_step.getOutputSource().generated.getPath(),
+                self.input_filename,
+                self.output_filename,
+            },
+        });
+        if (result.term.Exited != 0) {
+            std.debug.print("{s}\n", .{result.stdout});
+            std.debug.print("{s}\n", .{result.stderr});
+            return error.BadTestExitCode;
+        }
+    }
+};
--- scripts/test_runner.py
+++ scripts/test_runner.py
@@ -3,31 +3,52 @@ import subprocess
 import difflib
 import textwrap
 
-if len(sys.argv) is not 4:
-  sys.exit(1)
 
-binary = sys.argv[1]
-input_filename = sys.argv[2]
-output_filename = sys.argv[3]
+def main():
+    if len(sys.argv) != 4:
+        sys.exit(1)
 
-try:
-  with open(output_filename, "r") as f:
-    expected = f.read().strip()
-except FileNotFoundError:
-  print('output file does not exist')
-  sys.exit(1)
+    binary = sys.argv[1]
+    input_filename = sys.argv[2]
+    output_filename = sys.argv[3]
 
-proc = subprocess.run([binary, input_filename], stdout=subprocess.PIPE, encoding='utf-8')
-results = proc.stdout.strip()
+    try:
+        with open(output_filename, "r") as f:
+            expected = f.read().strip()
+    except FileNotFoundError:
+        print("output file does not exist")
+        sys.exit(1)
 
-if proc.returncode != 0:
-  print('process exited with non-zero return code')
-  sys.exit(1)
-if results != expected:
-  print('result differs from expected; context diff:\n')
-  diff = difflib.context_diff(expected.splitlines(), results.splitlines(), n=2, lineterm='', fromfile='expected', tofile='actual')
-  print(textwrap.indent('\n'.join(diff), '  '))
-  sys.exit(1)
+    proc = subprocess.run(
+        [binary, input_filename],
+        # We don't use stdin, but this function randomly fails when test_runner
+        # is called from a zig build script if we don't pipe it. This may be
+        # because zig passes "\Device\Null" when spawning a child process with
+        # ignored stdin, but need to investigate more.
+        stdin=subprocess.PIPE,
+        stdout=subprocess.PIPE,
+        encoding="utf-8",
+    )
+    results = proc.stdout.strip()
 
-sys.exit(0)
+    if proc.returncode != 0:
+        print("process exited with non-zero return code")
+        sys.exit(1)
+    if results != expected:
+        print("result differs from expected; context diff:\n")
+        diff = difflib.context_diff(
+            expected.splitlines(),
+            results.splitlines(),
+            n=2,
+            lineterm="",
+            fromfile="expected",
+            tofile="actual",
+        )
+        print(textwrap.indent("\n".join(diff), "  "))
+        sys.exit(1)
 
+    sys.exit(0)
+
+
+if __name__ == "__main__":
+    main()
