85c807427affd9cf823e76b0876348aaa3cc4cff
J.C. Moyer <jcmoyer32@gmail.com>
2022-12-03 13:02:36 -0500

    aoc2022: day 2 simplify to lut

--- AdventOfCode2022/src/day02.zig
+++ AdventOfCode2022/src/day02.zig
@@ -5,149 +5,54 @@ const runner = @import("runner.zig");
 pub const main = runner.defaultMain;
 
 pub fn solve(ps: *runner.PuzzleSolverState) !void {
-    var games = try GameList.loadFromStream(ps.allocator, ps.getPuzzleInputReader());
-    try ps.solution(games.totalScore());
-    try ps.solution(games.totalScore2());
+    const s = try Solution.calcFromStream(ps.getPuzzleInputReader());
+    try ps.solution(s.sum1);
+    try ps.solution(s.sum2);
 }
 
-const PlayResult = enum {
-    lost,
-    draw,
-    won,
-
-    fn scoreValue(self: PlayResult) u64 {
-        return switch (self) {
-            .lost => 0,
-            .draw => 3,
-            .won => 6,
-        };
-    }
+const lut_part1 = [_]u32{
+    1 + 3, // A X; choice points + win/lose points
+    1 + 0, // B X
+    1 + 6, // C X
+    2 + 6, // A Y
+    2 + 3, // B Y
+    2 + 0, // C Y
+    3 + 0, // A Z
+    3 + 6, // B Z
+    3 + 3, // C Z
 };
 
-const Play = enum {
-    rock,
-    paper,
-    scissors,
-
-    fn scoreValue(self: Play) u64 {
-        return switch (self) {
-            .rock => 1,
-            .paper => 2,
-            .scissors => 3,
-        };
-    }
-
-    fn beatingPlay(self: Play) Play {
-        return switch (self) {
-            .rock => .paper,
-            .paper => .scissors,
-            .scissors => .rock,
-        };
-    }
-
-    fn losingPlay(self: Play) Play {
-        return switch (self) {
-            .rock => .scissors,
-            .paper => .rock,
-            .scissors => .paper,
-        };
-    }
-
-    fn beats(self: Play, other: Play) bool {
-        return self == other.beatingPlay();
-    }
-
-    fn getPlayForResult(self: Play, desired: PlayResult) Play {
-        return switch (desired) {
-            .draw => self,
-            .won => self.beatingPlay(),
-            .lost => self.losingPlay(),
-        };
-    }
-
-    /// Returns the result if `a` were played against `b`.
-    fn result(a: Play, b: Play) PlayResult {
-        if (a == b) {
-            return .draw;
-        } else if (a.beats(b)) {
-            return .won;
-        } else {
-            return .lost;
-        }
-    }
-
-    fn parse(ch: u8) !Play {
-        return switch (ch) {
-            'A', 'X' => .rock,
-            'B', 'Y' => .paper,
-            'C', 'Z' => .scissors,
-            else => error.Unrecognized,
-        };
-    }
+const lut_part2 = [_]u32{
+    3 + 0, // same as above
+    1 + 0,
+    2 + 0,
+    1 + 3,
+    2 + 3,
+    3 + 3,
+    2 + 6,
+    3 + 6,
+    1 + 6,
 };
 
-const Game = struct {
-    elf: Play,
-    you: Play,
+const Solution = struct {
+    sum1: u32 = 0,
+    sum2: u32 = 0,
 
-    fn score(self: Game) u64 {
-        const result = self.you.result(self.elf);
-        return result.scoreValue() + self.you.scoreValue();
-    }
-
-    fn score2(self: Game) u64 {
-        const treat_as: PlayResult = switch (self.you) {
-            .rock => .lost,
-            .paper => .draw,
-            .scissors => .won,
-        };
-        const my_play = self.elf.getPlayForResult(treat_as);
-        const result = my_play.result(self.elf);
-        return result.scoreValue() + my_play.scoreValue();
-    }
-};
-
-const GameList = struct {
-    games: []Game,
-
-    fn totalScore(self: GameList) u64 {
-        var sum: u64 = 0;
-        for (self.games) |g| {
-            sum += g.score();
-        }
-        return sum;
-    }
-
-    fn totalScore2(self: GameList) u64 {
-        var sum: u64 = 0;
-        for (self.games) |g| {
-            sum += g.score2();
-        }
-        return sum;
-    }
-
-    fn deinit(self: *GameList, allocator: Allocator) void {
-        allocator.free(self.games);
-    }
-
-    fn loadFromStream(allocator: Allocator, reader: anytype) !GameList {
+    fn calcFromStream(reader: anytype) !Solution {
         var buf: [256]u8 = undefined;
-
-        var games = std.ArrayListUnmanaged(Game){};
-        errdefer games.deinit(allocator);
+        var solution = Solution{};
 
         while (try reader.readUntilDelimiterOrEof(&buf, '\n')) |line| {
             const stripped_line = std.mem.trimRight(u8, line, "\r\n");
-            var iter = std.mem.tokenize(u8, stripped_line, " ");
 
-            var game = try games.addOne(allocator);
-            game.elf = try Play.parse(iter.next().?[0]);
-            game.you = try Play.parse(iter.next().?[0]);
+            var game_id: u8 = 0;
+            game_id += stripped_line[0] - 'A';
+            game_id += 3 * (stripped_line[2] - 'X');
+            solution.sum1 += lut_part1[game_id];
+            solution.sum2 += lut_part2[game_id];
         }
 
-        return GameList{
-            .games = try games.toOwnedSlice(allocator),
-        };
+        return solution;
     }
 };
 
@@ -158,29 +63,17 @@ const example_input =
 ;
 
 test "part 1" {
-    var allocator = std.testing.allocator;
     var fbs = std.io.fixedBufferStream(example_input);
     var reader = fbs.reader();
 
-    var g = try GameList.loadFromStream(
-        allocator,
-        reader,
-    );
-    defer g.deinit(allocator);
-
-    try std.testing.expectEqual(@as(u64, 15), g.totalScore());
+    const s = try Solution.calcFromStream(reader);
+    try std.testing.expectEqual(@as(u64, 15), s.sum1);
 }
 
 test "part 2" {
-    var allocator = std.testing.allocator;
     var fbs = std.io.fixedBufferStream(example_input);
     var reader = fbs.reader();
 
-    var g = try GameList.loadFromStream(
-        allocator,
-        reader,
-    );
-    defer g.deinit(allocator);
-
-    try std.testing.expectEqual(@as(u64, 12), g.totalScore2());
+    const s = try Solution.calcFromStream(reader);
+    try std.testing.expectEqual(@as(u64, 12), s.sum2);
 }
