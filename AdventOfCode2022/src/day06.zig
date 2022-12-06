07781715467d9a8c99b0a9bc8452576944bea44a
J.C. Moyer <jcmoyer32@gmail.com>
2022-12-06 01:27:44 -0500

    aoc2022: day 6 cleanup

--- AdventOfCode2022/src/day06.zig
+++ AdventOfCode2022/src/day06.zig
@@ -5,14 +5,14 @@ const runner = @import("runner.zig");
 pub const main = runner.defaultMain;
 
 pub fn solve(ps: *runner.PuzzleSolverState) !void {
-    var s = try Solution.calcFromString(ps.input_text);
+    var s = Solution.calcFromString(ps.input_text);
     try ps.solution(s.part1);
     try ps.solution(s.part2);
 }
 
 const CharSet = u64;
 
-fn putCharSetAssumeCapacity(set: *CharSet, str: []const u8) void {
+fn putCharSet(set: *CharSet, str: []const u8) void {
     var new_set: CharSet = 0;
     for (str) |ch| {
         new_set |= (@as(u64, 1) << @intCast(u6, ch - 'a'));
@@ -22,42 +22,30 @@ fn putCharSetAssumeCapacity(set: *CharSet, str: []const u8) void {
 
 const Solution = struct {
     const no_solution = std.math.maxInt(usize);
-    part1: usize,
-    part2: usize,
+    part1: usize = no_solution,
+    part2: usize = no_solution,
 
-    fn calcFromString(str: []const u8) !Solution {
-        var set_4: u64 = 0;
-        var set_14: u64 = 0;
-        var start: usize = 0;
-        var part1: usize = no_solution;
-        var part2: usize = no_solution;
-        while (start < str.len) : (start += 1) {
-            if (part1 == no_solution) {
-                putCharSetAssumeCapacity(&set_4, str[start .. start + 4]);
-                if (@popCount(set_4) == 4) {
-                    part1 = start + 4;
-                }
+    fn isPacketStart(packet: []const u8, packet_width: usize) bool {
+        var set: CharSet = 0;
+        putCharSet(&set, packet[0..packet_width]);
+        return @popCount(set) == packet_width;
+    }
+
+    fn calcFromString(str: []const u8) Solution {
+        var sol = Solution{};
+        for (str) |_, start| {
+            const slice = str[start..];
+            if (sol.part1 == no_solution and isPacketStart(slice, 4)) {
+                sol.part1 = start + 4;
             }
-            if (part2 == no_solution) {
-                putCharSetAssumeCapacity(&set_14, str[start .. start + 14]);
-                if (@popCount(set_14) == 14) {
-                    part2 = start + 14;
-                }
+            if (sol.part2 == no_solution and isPacketStart(slice, 14)) {
+                sol.part2 = start + 14;
             }
-            if (part1 != no_solution and part2 != no_solution) {
+            if (sol.part1 != no_solution and sol.part2 != no_solution) {
                 break;
             }
         }
-        if (part1 == no_solution) {
-            return error.NoPart1;
-        }
-        if (part2 == no_solution) {
-            return error.NoPart2;
-        }
-        return Solution{
-            .part1 = part1,
-            .part2 = part2,
-        };
+        return sol;
     }
 };
 
@@ -66,7 +54,7 @@ const example_input =
 ;
 
 test "parts 1 and 2" {
-    var s = try Solution.calcFromString(example_input);
+    var s = Solution.calcFromString(example_input);
     try std.testing.expectEqual(@as(usize, 7), s.part1);
     try std.testing.expectEqual(@as(usize, 19), s.part2);
 }
