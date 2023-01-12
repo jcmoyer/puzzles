2afe3e529482f10111afc21e25b00fce74fb1b9d
J.C. Moyer <jcmoyer32@gmail.com>
2023-01-12 14:46:34 -0500

    aoc2015: reduce day01

--- AdventOfCode2015/src/day01.jl
+++ AdventOfCode2015/src/day01.jl
@@ -4,21 +4,9 @@ include("sf.jl")
 using .Supafast
 
 function solve(text)
-    floor = 0
-    p2 = nothing
-    for i in eachindex(text)
-        c = text[i]
-        if c == '('
-            floor += 1
-        else
-            floor -= 1
-            if floor == -1 && p2 === nothing
-                p2 = i
-            end
-        end
-    end
-    println(floor)
-    println(p2)
+    ints = map(ch -> ch == '(' ? 1 : -1, collect(text))
+    println(sum(ints))
+    println(findfirst(x -> x == -1, accumulate(+, ints)))
 end
 
 solve(getinput(2015, 1))
