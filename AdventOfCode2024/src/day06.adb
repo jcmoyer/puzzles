7bba2cbe664ef8c0a667a1535c442d09102ed8e1
J.C. Moyer <jcmoyer32@gmail.com>
2024-12-08 21:58:40 -0500

    aoc2024-06: optimize
    Reduces runtime by roughly a factor of 10x in both Debug and Release
    modes from 3 seconds to 300 milliseconds.
    
    Part 1 and part 2 solvers are split since they need differing amounts of
    information.
    
    Part 1 now records a list of distinct, visited locations and returns it
    for use by the part 2 solver.
    
    Only part 1 needs to count the number of visited locations. Instead of
    counting in linear time, it now counts as new locations are encountered.
    
    The 3-way cache in part 2 now uses only Cardinal_Directions instead of
    the full range of Directions. This probably has a minor effect on cache
    usage.
    
    Obstacle placement in part 2 now only uses locations the guard visited
    in part 1. This significantly reduces the number of simulations.

--- AdventOfCode2024/src/day06.adb
+++ AdventOfCode2024/src/day06.adb
@@ -1,25 +1,69 @@
 with Advent.IO;                  use Advent.IO;
 with Advent.Integer_Vector_Math; use Advent.Integer_Vector_Math;
 with Advent.Directions;          use Advent.Directions;
+with Ada.Containers.Vectors;
 with Ada.Command_Line;
 
 procedure Day06 is
 
-   type Visited_Map is array (Integer range <>, Integer range <>, Direction range <>) of Boolean;
+   type Visited_Map is array (Integer range <>, Integer range <>) of Boolean;
 
-   function Simulate (Map : Char_Matrix; Start : Vec2) return Integer is
-      Visited : Visited_Map (Map'Range (1), Map'Range (2), Direction) :=
+   type Visited_Directional_Map is
+     array (Integer range <>, Integer range <>, Cardinal_Direction range <>) of Boolean;
+
+   package Location_Vectors is new Ada.Containers.Vectors
+     (Index_Type => Positive, Element_Type => Vec2);
+
+   subtype Location_Vector is Location_Vectors.Vector;
+
+   --  Returns the number of distinct locations visited and stores those
+   --  locations in `Visited_Locations`. Assumes the input terminates.
+   function Simulate_To_Termination
+     (Map : Char_Matrix; Start : Vec2; Visited_Locations : in out Location_Vector) return Natural
+   is
+      Visited : Visited_Map (Map'Range (1), Map'Range (2)) := (others => (others => False));
+
+      Pos : Vec2               := Start;
+      Dir : Cardinal_Direction := North;
+   begin
+      Visited_Locations.Clear;
+
+      while In_Bounds (Map, Pos) loop
+         if not Visited (Pos (X), Pos (Y)) then
+            Visited (Pos (X), Pos (Y)) := True;
+            Visited_Locations.Append (Vec2'(Pos (X), Pos (Y)));
+         end if;
+
+         declare
+            Next_Pos : constant Vec2 := Pos + To_Vector (Dir);
+         begin
+            if In_Bounds (Map, Next_Pos) and then Element (Map, Next_Pos) = '#' then
+               Dir := Rotate_CW_90 (Dir);
+            else
+               Pos := Next_Pos;
+            end if;
+         end;
+      end loop;
+
+      return Natural (Visited_Locations.Length);
+   end Simulate_To_Termination;
+
+   type Loop_Result is (Looping, Terminating);
+
+   --  Simplified version of the above function that only checks for loops -
+   --  doesn't save the path.
+   function Simulate_For_Loop (Map : Char_Matrix; Start : Vec2) return Loop_Result is
+      Visited : Visited_Directional_Map (Map'Range (1), Map'Range (2), Cardinal_Direction) :=
         (others => (others => (others => False)));
 
-      Pos   : Vec2               := Start;
-      Dir   : Cardinal_Direction := North;
-      Steps : Natural            := 0;
+      Pos : Vec2               := Start;
+      Dir : Cardinal_Direction := North;
    begin
       while In_Bounds (Map, Pos) loop
          --  if we were at this place before facing the same direction, the
          --  path will definitely repeat
          if Visited (Pos (X), Pos (Y), Dir) then
-            return -1;
+            return Looping;
          end if;
          Visited (Pos (X), Pos (Y), Dir) := True;
 
@@ -34,19 +78,8 @@ procedure Day06 is
          end;
       end loop;
 
-      for I in Visited'Range (1) loop
-         for J in Visited'Range (2) loop
-            for K in Cardinal_Direction loop
-               if Visited (I, J, K) then
-                  Steps := Steps + 1;
-                  exit;
-               end if;
-            end loop;
-         end loop;
-      end loop;
-
-      return Steps;
-   end Simulate;
+      return Terminating;
+   end Simulate_For_Loop;
 
    No_Start : exception;
 
@@ -62,31 +95,30 @@ procedure Day06 is
       raise No_Start with "no starting position for guard";
    end Find_Start;
 
-   function Simulate_Count_Loops (Map : Char_Matrix; Start : Vec2) return Natural is
+   function Simulate_Count_Loops
+     (Map : Char_Matrix; Start : Vec2; Obstacles : Location_Vector) return Natural
+   is
       Loops : Natural := 0;
    begin
-      for I in Map'Range (1) loop
-         for J in Map'Range (2) loop
-            if Map (I, J) = '.' then
-               declare
-                  Modified_Map : Char_Matrix := Map;
-               begin
-                  Modified_Map (I, J) := '#';
-                  if Simulate (Modified_Map, Start) = -1 then
-                     Loops := Loops + 1;
-                  end if;
-               end;
+      for Location of Obstacles loop
+         declare
+            Modified_Map : Char_Matrix := Map;
+         begin
+            Modified_Map (Location (X), Location (Y)) := '#';
+            if Simulate_For_Loop (Modified_Map, Start) = Looping then
+               Loops := Loops + 1;
             end if;
-         end loop;
+         end;
       end loop;
       return Loops;
    end Simulate_Count_Loops;
 
    Map : constant Char_Matrix := Advent.IO.Read_Tilemap (Ada.Command_Line.Argument (1));
 
-   Start : constant Vec2 := Find_Start (Map);
+   Start   : constant Vec2 := Find_Start (Map);
+   Visited : Location_Vector;
 
 begin
-   Solution (Simulate (Map, Start));
-   Solution (Simulate_Count_Loops (Map, Start));
+   Solution (Simulate_To_Termination (Map, Start, Visited));
+   Solution (Simulate_Count_Loops (Map, Start, Visited));
 end Day06;
