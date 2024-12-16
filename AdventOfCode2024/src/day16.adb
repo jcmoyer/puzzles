47143b1ddcefa139dcfb4799851258511d600352
J.C. Moyer <jcmoyer32@gmail.com>
2024-12-16 14:43:54 -0500

    aoc2024-16: second optimization pass
    Reduces runtime from 930ms to 35ms. The main optimization this time is
    reducing vector copies.
    
    Instead of storing the full path as a vector in the path state, we store
    the path externally and refer to them by an integer index instead. This
    design also makes it easy to deduplicate paths. Instead of copying the
    whole path from start to finish like before, we only need to allocate a
    new position and parent it to the path we came from.
    
    Additionally, we no longer copy tile position vectors when considering
    edges in the graph. We introduce `Junction_Out_Edge_Ptr` for this
    purpose, as accessing the edge through an access type doesn't copy its
    vector.

--- AdventOfCode2024/src/day16.adb
+++ AdventOfCode2024/src/day16.adb
@@ -13,8 +13,6 @@ procedure Day16 is
 
    Map : Char_Matrix := Read_Tilemap (Ada.Command_Line.Argument (1));
 
-   M_Start, M_End : Vec2;
-
    package Vec2_Vectors is new Ada.Containers.Vectors (Positive, Vec2);
    package Vec2_Sets is new Ada.Containers.Hashed_Sets
      (Vec2, Hash => Hash, Equivalent_Elements => "=");
@@ -24,6 +22,8 @@ procedure Day16 is
    package Junction_Id_Maps is new Ada.Containers.Hashed_Maps
      (Vec2, Junction_Id, Hash => Hash, Equivalent_Keys => "=");
 
+   J_Start, J_End : Junction_Id;
+
    type Junction_Out_Edge is record
       Destination : Junction_Id;
       Steps       : Vec2_Vectors.Vector := Vec2_Vectors.Empty_Vector;
@@ -31,7 +31,13 @@ procedure Day16 is
       Out_Facing  : Cardinal_Direction;
    end record;
 
-   type Junction_Out_Edges is array (Cardinal_Direction) of Junction_Out_Edge;
+   type Junction_Out_Edges is array (Cardinal_Direction) of aliased Junction_Out_Edge;
+
+   --  When we inspect the edges during the search, we use pointers to avoid
+   --  copying the edge's tile vector. This has a measurable effect on
+   --  performance; about ~10-20% runtime savings depending on optimization
+   --  level on official inputs.
+   type Junction_Out_Edge_Ptr is access constant Junction_Out_Edge;
 
    type Junction is record
       Id    : Junction_Id;
@@ -40,14 +46,13 @@ procedure Day16 is
    end record;
 
    type Path_Record is record
-      Pos   : Junction_Id;
-      Dir   : Direction;
-      Score : Integer;
+      Parent : Natural;
+      Pos    : Junction_Id;
+      Dir    : Cardinal_Direction;
+      Score  : Integer;
    end record;
 
    package Path_Record_Vectors is new Ada.Containers.Vectors (Positive, Path_Record);
-
-   package Junction_Id_Vectors is new Ada.Containers.Vectors (Positive, Junction_Id);
 
    package Junction_Vectors is new Ada.Containers.Vectors (Positive, Junction);
    Junctions    : Junction_Vectors.Vector;
@@ -57,7 +62,7 @@ procedure Day16 is
       Score : Integer := 0;
       Pos   : Junction_Id;
       Dir   : Cardinal_Direction;
-      Path  : Path_Record_Vectors.Vector;
+      Path  : Natural;
    end record;
 
    type Search_Result is record
@@ -70,12 +75,12 @@ procedure Day16 is
    package PS_Queues is new Advent.Containers.Priority_Queues
      (Element_Type => Path_State, "<" => Order_Score);
 
-   type Score_Map is array (Junction_Id range <>, Direction range <>) of Integer with
+   type Score_Map is array (Junction_Id range <>, Cardinal_Direction range <>) of Integer with
      Default_Component_Value => Integer'Last;
 
    function Cost (From, To : Junction_Id) return Integer is
    begin
-      for D in Direction loop
+      for D in Cardinal_Direction loop
          if Junctions (From).Edges (D).Destination = To then
             return Junctions (From).Edges (D).Cost;
          end if;
@@ -125,64 +130,94 @@ procedure Day16 is
       B          : Integer := Integer'Last;
       M          : Score_Map (Junctions.First_Index .. Junctions.Last_Index, Cardinal_Direction);
       Best_Tiles : Vec2_Sets.Set;
+
+      Records : Path_Record_Vectors.Vector;
+
+      function Fork_Path (State : Path_State) return Positive is
+      begin
+         Records.Append
+           ((Parent => State.Path, Pos => State.Pos, Dir => State.Dir, Score => State.Score));
+         return Records.Last_Index;
+      end Fork_Path;
+
+      function Parent_Path (Path : Positive) return Natural is
+      begin
+         return Records (Path).Parent;
+      end Parent_Path;
+
    begin
-      Q.Enqueue
-        (Path_State'
-           (Pos   => Junction_Ids.Element (M_Start),
-            Dir   => East,
-            Score => 0,
-            Path  => Path_Record_Vectors.Empty_Vector));
+      Q.Enqueue (Path_State'(Pos => J_Start, Dir => East, Score => 0, Path => 0));
 
       while Q.Length > 0 loop
          Q.Dequeue (C);
-         C.Path.Append (Path_Record'(Pos => C.Pos, Dir => C.Dir, Score => C.Score));
+         C.Path := Fork_Path (C);
 
-         if C.Score <= B and then C.Pos = Junction_Ids.Element (M_End) then
+         if C.Score <= B and then C.Pos = J_End then
             B := C.Score;
-            for I in C.Path.First_Index .. C.Path.Last_Index - 1 loop
-               Gather_Steps (C.Path (I).Pos, C.Path (I + 1).Pos, Best_Tiles);
-            end loop;
+            declare
+               P : Natural := C.Path;
+            begin
+               while Parent_Path (P) /= 0 loop
+                  Gather_Steps (Records (P).Pos, Records (Parent_Path (P)).Pos, Best_Tiles);
+                  P := Parent_Path (P);
+               end loop;
+            end;
          end if;
 
-         if C.Pos /= Junction_Ids.Element (M_End) and then C.Score <= M (C.Pos, C.Dir)
-           and then C.Score <= B
-         then
+         if C.Pos /= J_End and then C.Score <= M (C.Pos, C.Dir) and then C.Score <= B then
             if C.Score <= M (C.Pos, C.Dir) then
                M (C.Pos, C.Dir) := C.Score;
             end if;
 
-            if Junctions (C.Pos).Edges (C.Dir).Destination /= 0 then
-               Q.Enqueue
-                 ((Score => C.Score + Junctions (C.Pos).Edges (C.Dir).Cost,
-                   Pos   => Junctions (C.Pos).Edges (C.Dir).Destination,
-                   Dir   => Junctions (C.Pos).Edges (C.Dir).Out_Facing,
-                   Path  => C.Path));
-            end if;
+            declare
+               New_State : Path_State;
 
-            if Junctions (C.Pos).Edges (Rotate_CW_90 (C.Dir)).Destination /= 0 then
-               Q.Enqueue
-                 ((Score => C.Score + Junctions (C.Pos).Edges (Rotate_CW_90 (C.Dir)).Cost + 1_000,
-                   Pos   => Junctions (C.Pos).Edges (Rotate_CW_90 (C.Dir)).Destination,
-                   Dir   => Junctions (C.Pos).Edges (Rotate_CW_90 (C.Dir)).Out_Facing,
-                   Path  => C.Path));
-            end if;
+               Edge : constant Junction_Out_Edge_Ptr := Junctions (C.Pos).Edges (C.Dir)'Access;
+            begin
+               if Edge.Destination /= 0 then
+                  New_State.Pos   := Edge.Destination;
+                  New_State.Dir   := Edge.Out_Facing;
+                  New_State.Score := C.Score + Edge.Cost;
+                  New_State.Path  := C.Path;
+                  Q.Enqueue (New_State);
+               end if;
+            end;
 
-            if Junctions (C.Pos).Edges (Rotate_CCW_90 (C.Dir)).Destination /= 0 then
-               Q.Enqueue
-                 ((Score => C.Score + Junctions (C.Pos).Edges (Rotate_CCW_90 (C.Dir)).Cost + 1_000,
-                   Pos   => Junctions (C.Pos).Edges (Rotate_CCW_90 (C.Dir)).Destination,
-                   Dir   => Junctions (C.Pos).Edges (Rotate_CCW_90 (C.Dir)).Out_Facing,
-                   Path  => C.Path));
-            end if;
+            declare
+               New_State : Path_State;
 
+               Edge : constant Junction_Out_Edge_Ptr :=
+                 Junctions (C.Pos).Edges (Rotate_CW_90 (C.Dir))'Access;
+            begin
+               if Edge.Destination /= 0 then
+                  New_State.Pos   := Edge.Destination;
+                  New_State.Dir   := Edge.Out_Facing;
+                  New_State.Score := C.Score + Edge.Cost + 1_000;
+                  New_State.Path  := C.Path;
+                  Q.Enqueue (New_State);
+               end if;
+            end;
+
+            declare
+               New_State : Path_State;
+
+               Edge : constant Junction_Out_Edge_Ptr :=
+                 Junctions (C.Pos).Edges (Rotate_CCW_90 (C.Dir))'Access;
+            begin
+               if Edge.Destination /= 0 then
+                  New_State.Pos   := Edge.Destination;
+                  New_State.Dir   := Edge.Out_Facing;
+                  New_State.Score := C.Score + Edge.Cost + 1_000;
+                  New_State.Path  := C.Path;
+                  Q.Enqueue (New_State);
+               end if;
+            end;
          end if;
       end loop;
 
       return (Shortest_Path => B, Total_Tiles => Integer (Best_Tiles.Length));
 
    end Search;
-
-   Junc, N : Integer := 0;
 
    function Find_Junction_Down_Path
      (Start : Vec2; Dir : Cardinal_Direction) return Junction_Out_Edge
@@ -221,8 +256,7 @@ procedure Day16 is
          Out_Facing  => CDir);
    end Find_Junction_Down_Path;
 
-   procedure Connect is
-
+   procedure Connect_Junctions is
    begin
       for Id in Junctions.First_Index .. Junctions.Last_Index loop
          for D in Cardinal_Direction loop
@@ -232,63 +266,56 @@ procedure Day16 is
             end if;
          end loop;
       end loop;
-   end Connect;
+   end Connect_Junctions;
+
+   function Alloc_Junction (Where : Vec2) return Junction_Id is
+   begin
+      Junctions.Append
+        (Junction'
+           (Id    => Junctions.Last_Index + 1,
+            Pos   => Where,
+            Edges => (others => (Destination => 0, Steps => <>, Cost => 0, Out_Facing => <>))));
+      Junction_Ids.Include (Where, Junctions.Last_Index);
+      return Junctions.Last_Index;
+   end Alloc_Junction;
+
+   procedure Find_Junctions is
+      N      : Integer := 0;
+      Unused : Junction_Id;
+   begin
+      for I in Map'Range (1) loop
+         for J in Map'Range (2) loop
+            if Map (I, J) = '.' then
+               N := 0;
+               for D in Cardinal_Direction loop
+                  if Element (Map, To_Vector (D) + (I, J)) = '.' then
+                     N := N + 1;
+                  end if;
+               end loop;
+               if N >= 3 then
+                  Unused := Alloc_Junction ((I, J));
+               end if;
+            elsif Map (I, J) = 'S' then
+               J_Start    := Alloc_Junction ((I, J));
+               --  For the purposes of connecting junctions later, the start
+               --  and end tiles need to become empty spaces.
+               Map (I, J) := '.';
+            elsif Map (I, J) = 'E' then
+               J_End      := Alloc_Junction ((I, J));
+               Map (I, J) := '.';
+            end if;
+         end loop;
+      end loop;
+   end Find_Junctions;
 
 begin
+   Find_Junctions;
+   Connect_Junctions;
 
-   for I in Map'Range (1) loop
-      for J in Map'Range (2) loop
-         if Map (I, J) = '.' then
-            N := 0;
-            for D in Cardinal_Direction loop
-               if Element (Map, To_Vector (D) + (I, J)) = '.' then
-                  N := N + 1;
-               end if;
-            end loop;
-            if N >= 3 then
-               Junctions.Append
-                 (Junction'
-                    (Id    => Junctions.Last_Index + 1,
-                     Pos   => (I, J),
-                     Edges =>
-                       (others => (Destination => 0, Steps => <>, Cost => 0, Out_Facing => <>))));
-               Junction_Ids.Include ((I, J), Junctions.Last_Index);
-            end if;
-         end if;
-      end loop;
-   end loop;
-
-   for I in Map'Range (1) loop
-      for J in Map'Range (2) loop
-         if Map (I, J) = 'S' then
-            M_Start    := (I, J);
-            Map (I, J) := '.';
-         end if;
-         if Map (I, J) = 'E' then
-            M_End      := (I, J);
-            Map (I, J) := '.';
-         end if;
-      end loop;
-   end loop;
-
-   Junctions.Append
-     (Junction'
-        (Id    => Junctions.Last_Index + 1,
-         Pos   => M_Start,
-         Edges => (others => (Destination => 0, Steps => <>, Cost => 0, Out_Facing => <>))));
-   Junction_Ids.Include (M_Start, Junctions.Last_Index);
-
-   Junctions.Append
-     (Junction'
-        (Id      => Junctions.Last_Index + 1,
-         Pos     => M_End,
-           Edges => (others => (Destination => 0, Steps => <>, Cost => 0, Out_Facing => <>))));
-   Junction_Ids.Include (M_End, Junctions.Last_Index);
-
-   Connect;
+   Put_Line (Standard_Error, "Found " & Junctions.Length'Image & " junctions");
 
    declare
-      Result : Search_Result := Search;
+      Result : constant Search_Result := Search;
    begin
       Solution (Result.Shortest_Path);
       Solution (Result.Total_Tiles);
