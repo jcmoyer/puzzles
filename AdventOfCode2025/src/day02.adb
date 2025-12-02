9b148ee1f6714445521860e9a2e45a892864673c
J.C. Moyer <jcmoyer32@gmail.com>
2025-12-02 01:07:53 -0500

    aoc2025: day 2

---
+++ AdventOfCode2025/src/day02.adb
@@ -0,0 +1,77 @@
+with Ada.Strings.Fixed; use Ada.Strings.Fixed;
+with Ada.Strings;       use Ada.Strings;
+with Advent.IO;
+with Advent.Strings;
+with Ada.Command_Line;
+
+procedure Day02 is
+   function Is_Invalid (S : String; First_Chunk_Size : Positive) return Boolean
+   is
+      Chunk_Size  : Positive := First_Chunk_Size;
+      Chunk_Count : Integer;
+
+      function Is_Valid_Chunk_Size return Boolean
+      is (S'Length rem Chunk_Size = 0);
+
+      --!format off
+      function Contains_Duplicate_Chunks return Boolean
+      --  true if the first chunk in S is equal to every subsequent chunk
+      is (for all I in 1 .. Chunk_Count - 1 =>
+            S (S'First .. S'First + Chunk_Size - 1)
+            =
+            S (S'First + I * Chunk_Size .. S'First + I * Chunk_Size + Chunk_Size - 1));
+      --!format on
+   begin
+      while Chunk_Size <= S'Length / 2 loop
+         Chunk_Count := S'Length / Chunk_Size;
+
+         if Is_Valid_Chunk_Size and then Contains_Duplicate_Chunks then
+            return True;
+         end if;
+
+         Chunk_Size := Chunk_Size + 1;
+      end loop;
+
+      return False;
+   end Is_Invalid;
+
+   ----------------------------------------------------------------------------
+
+   Lines : constant Advent.Strings.String_Array :=
+     Advent.IO.Read_All_Lines (Ada.Command_Line.Argument (1));
+
+   Ints : constant Advent.Strings.String_Array :=
+     Advent.Strings.Split_Any (S => Lines.First_Element, Delims => "-,");
+
+   I : Integer := Ints.First_Index;
+
+   P1, P2 : Long_Long_Integer := 0;
+
+   First, Last : Long_Long_Integer;
+begin
+   while I in Ints.First_Index .. Ints.Last_Index loop
+      First := Long_Long_Integer'Value (Ints (I));
+      Last := Long_Long_Integer'Value (Ints (I + 1));
+
+      for Int in First .. Last loop
+         declare
+            S : constant String := Trim (Int'Image, Left);
+         begin
+            if S'Length rem 2 = 0
+              and then Is_Invalid (S, First_Chunk_Size => S'Length / 2)
+            then
+               P1 := P1 + Int;
+            end if;
+
+            if Is_Invalid (S, First_Chunk_Size => 1) then
+               P2 := P2 + Int;
+            end if;
+         end;
+      end loop;
+
+      I := I + 2;
+   end loop;
+
+   Advent.IO.Solution (P1);
+   Advent.IO.Solution (P2);
+end Day02;
