445a0528dccd99aacbc0ebe805ea1bb84177f3f3
J.C. Moyer <jcmoyer32@gmail.com>
2025-12-06 11:26:01 -0500

    aoc2023-util: deduplicate integer parsing code

--- AdventOfCode2023/advent/src/advent-parsers-integers.adb
+++ AdventOfCode2023/advent/src/advent-parsers-integers.adb
@@ -2,9 +2,8 @@ with Ada.Characters.Handling; use Ada.Characters.Handling;
 
 package body Advent.Parsers.Integers is
 
-   function Extract_Positive_Integers (S : String) return Vector is
-      I      : Positive := S'First;
-      Result : Vector;
+   procedure Iterate_Integers (S : String; Callback : access procedure (Span : String)) is
+      I : Positive := S'First;
    begin
       while I <= S'Last loop
          if Is_Digit (S (I)) then
@@ -14,28 +13,7 @@ package body Advent.Parsers.Integers is
                while I <= S'Last and then Is_Digit (S (I)) loop
                   I := I + 1;
                end loop;
-               Result.Append (Element_Type'Value (S (Int_Start .. I - 1)));
-            end;
-         else
-            I := I + 1;
-         end if;
-      end loop;
-      return Result;
-   end Extract_Positive_Integers;
-
-   function Extract_Integers (S : String) return Vector is
-      I      : Positive := S'First;
-      Result : Vector;
-   begin
-      while I <= S'Last loop
-         if Is_Digit (S (I)) then
-            declare
-               Int_Start : constant Positive := I;
-            begin
-               while I <= S'Last and then Is_Digit (S (I)) loop
-                  I := I + 1;
-               end loop;
-               Result.Append (Element_Type'Value (S (Int_Start .. I - 1)));
+               Callback (S (Int_Start .. I - 1));
             end;
          elsif S (I) = '-' and then I + 1 in S'Range and then Is_Digit (S (I + 1)) then
             --  handle negative numbers
@@ -47,74 +25,73 @@ package body Advent.Parsers.Integers is
                while I <= S'Last and then Is_Digit (S (I)) loop
                   I := I + 1;
                end loop;
-               Result.Append (Element_Type'Value (S (Int_Start .. I - 1)));
+               Callback (S (Int_Start .. I - 1));
             end;
          else
             I := I + 1;
          end if;
       end loop;
+   end Iterate_Integers;
+
+   function Extract_Positive_Integers (S : String) return Vector is
+      Result : Vector;
+
+      procedure Append_Result (Span : String) is
+      begin
+         if Span (Span'First) = '-' then
+            Result.Append (Element_Type'Value (Span (Span'First + 1 .. Span'Last)));
+         else
+            Result.Append (Element_Type'Value (Span));
+         end if;
+      end Append_Result;
+   begin
+      Iterate_Integers (S, Append_Result'Access);
+      return Result;
+   end Extract_Positive_Integers;
+
+   function Extract_Integers (S : String) return Vector is
+      Result : Vector;
+
+      procedure Append_Result (Span : String) is
+      begin
+         Result.Append (Element_Type'Value (Span));
+      end Append_Result;
+   begin
+      Iterate_Integers (S, Append_Result'Access);
       return Result;
    end Extract_Integers;
 
    function Extract_Positive_Integers (S : String; Output : out Array_Type) return Natural is
       Read         : Natural := 0;
-      I            : Positive := S'First;
       Output_Index : Positive := Output'First;
-   begin
-      while I <= S'Last loop
-         if Is_Digit (S (I)) then
-            declare
-               Int_Start : constant Positive := I;
-            begin
-               while I <= S'Last and then Is_Digit (S (I)) loop
-                  I := I + 1;
-               end loop;
-               Output (Output_Index) := Element_Type'Value (S (Int_Start .. I - 1));
-               Output_Index := Output_Index + 1;
-               Read := Read + 1;
-            end;
+
+      procedure Append_Result (Span : String) is
+      begin
+         if Span (Span'First) = '-' then
+            Output (Output_Index) := Element_Type'Value (Span (Span'First + 1 .. Span'Last));
          else
-            I := I + 1;
+            Output (Output_Index) := Element_Type'Value (Span);
          end if;
-      end loop;
+         Output_Index := Output_Index + 1;
+         Read := Read + 1;
+      end Append_Result;
+   begin
+      Iterate_Integers (S, Append_Result'Access);
       return Read;
    end Extract_Positive_Integers;
 
    function Extract_Integers (S : String; Output : out Array_Type) return Natural is
       Read         : Natural := 0;
-      I            : Positive := S'First;
       Output_Index : Positive := Output'First;
+
+      procedure Append_Result (Span : String) is
+      begin
+         Output (Output_Index) := Element_Type'Value (Span);
+         Output_Index := Output_Index + 1;
+         Read := Read + 1;
+      end Append_Result;
    begin
-      while I <= S'Last loop
-         if Is_Digit (S (I)) then
-            declare
-               Int_Start : constant Positive := I;
-            begin
-               while I <= S'Last and then Is_Digit (S (I)) loop
-                  I := I + 1;
-               end loop;
-               Output (Output_Index) := Element_Type'Value (S (Int_Start .. I - 1));
-               Output_Index := Output_Index + 1;
-               Read := Read + 1;
-            end;
-         elsif S (I) = '-' and then I + 1 in S'Range and then Is_Digit (S (I + 1)) then
-            --  handle negative numbers
-            declare
-               Int_Start : constant Positive := I;
-            begin
-               --  move cursor past sign to first digit
-               I := I + 1;
-               while I <= S'Last and then Is_Digit (S (I)) loop
-                  I := I + 1;
-               end loop;
-               Output (Output_Index) := Element_Type'Value (S (Int_Start .. I - 1));
-               Output_Index := Output_Index + 1;
-               Read := Read + 1;
-            end;
-         else
-            I := I + 1;
-         end if;
-      end loop;
+      Iterate_Integers (S, Append_Result'Access);
       return Read;
    end Extract_Integers;
 
--- AdventOfCode2023/advent/tests/src/test_integer_parsers.adb
+++ AdventOfCode2023/advent/tests/src/test_integer_parsers.adb
@@ -1,10 +1,14 @@
 with Advent.Integer_Parsers; use Advent.Integer_Parsers;
 with Advent.Testing;         use Advent.Testing;
+with Ada.Containers;
 
 procedure Test_Integer_Parsers is
 
    Arr : Advent.Integer_Parsers.Array_Type (1 .. 16);
    N   : Integer := 0;
+
+   Vec : Advent.Integer_Parsers.Vector;
+   use type Ada.Containers.Count_Type;
 
 begin
 
@@ -13,13 +17,27 @@ begin
    Assert (Arr (1) = 1);
    Assert (Arr (2) = -2);
 
+   Vec := Extract_Integers ("1-2");
+   Assert (Vec.Length = 2);
+   Assert (Vec (1) = 1);
+   Assert (Vec (2) = -2);
+
    N := Extract_Positive_Integers ("1-2", Arr);
    Assert (N = 2);
    Assert (Arr (1) = 1);
    Assert (Arr (2) = 2);
 
+   Vec := Extract_Positive_Integers ("1-2");
+   Assert (Vec.Length = 2);
+   Assert (Vec (1) = 1);
+   Assert (Vec (2) = 2);
+
    N := Extract_Integers ("1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16", Arr);
    Assert (N = 16);
    Assert (for all I in 1 .. 16 => Arr (I) = I);
+
+   Vec := Extract_Integers ("1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16");
+   Assert (Vec.Length = 16);
+   Assert (for all I in 1 .. 16 => Vec (I) = I);
 
 end Test_Integer_Parsers;
